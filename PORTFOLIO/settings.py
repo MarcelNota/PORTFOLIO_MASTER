cb2b3e27bf96ced9b77fb108945fa16604b0b7f3
MarcelNota <n3gr1t0.mn@gmail.com>
2024-11-22 12:18:20 +0200

    v2

--- PORTFOLIO/settings.py
+++ PORTFOLIO/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -77,9 +78,11 @@ WSGI_APPLICATION = 'PORTFOLIO.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'portfolioDB',
+        'NAME': 'portfoliodb',
         'USER': 'postgres',
-        'PASSWORD': 'marcel'
+        'PASSWORD': 'marcel',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
